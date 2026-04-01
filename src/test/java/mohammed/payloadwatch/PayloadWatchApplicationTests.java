c5cb15a7186ed8b8a94022d95245b2754f44dfe4
momoElbn <mohammed.elouaabani@gmail.com>
2026-04-01 17:37:38 -0400

    Initial Spring Boot project commit

---
+++ src/test/java/mohammed/payloadwatch/PayloadWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package mohammed.payloadwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayloadWatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
