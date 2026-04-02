92c099bbd836e3632fc8db3dc67bb3e4a9401826
momoElbn <mohammed.elouaabani@gmail.com>
2026-04-02 14:16:04 -0400

    Implemented repositories and queries

--- src/main/java/mohammed/payloadwatch/repositories/UserRepository.java
+++ src/main/java/mohammed/payloadwatch/repositories/UserRepository.java
@@ -1,4 +1,11 @@
 package mohammed.payloadwatch.repositories;
 
-public interface UserRepository {
+import mohammed.payloadwatch.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    //Find a user by their Cognito sub
+    Optional<User> findByCognitoSub(String cognitoSub);
 }
